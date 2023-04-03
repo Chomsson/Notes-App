ab830f1dda853515e833b070cadd29dd7058269c
Piotr Maksam <pmaksam@gmail.com>
2023-04-03 17:22:43 +0200

    added some basic content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
+import { ControlPanelComponent } from './control-panel/control-panel.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    ControlPanelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
